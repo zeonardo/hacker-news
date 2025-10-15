96239370e7e50798655367d37c6a8b31df65361f
Leonardo Lima <leonardo@powertofly.com>
2025-10-15 18:21:38 -0300

    Fix Jest configuration for CI deployment

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,11 @@ export default {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy", // Mock CSS imports
   },
   transform: {
-    "^.+\\.tsx?$": "ts-jest", // Use ts-jest for TypeScript files
+    "^.+\\.tsx?$": ["ts-jest", {
+      useESM: true,
+    }], // Use ts-jest for TypeScript files
   },
   extensionsToTreatAsEsm: [".ts", ".tsx"], // Treat TypeScript files as ES modules
   testPathIgnorePatterns: ["/node_modules/", "/dist/"], // Ignore these paths
+  testTimeout: 10000, // Increase timeout for CI
 };
