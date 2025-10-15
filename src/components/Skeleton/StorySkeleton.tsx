bc2472906565ecf0be1098b236e19d770d25d5d4
Leonardo Lima <leonardo@powertofly.com>
2025-10-15 18:49:05 -0300

    fix: fix build and update pre-commit lint and tsc check

--- src/components/Skeleton/StorySkeleton.tsx
+++ src/components/Skeleton/StorySkeleton.tsx
@@ -5,6 +5,8 @@ interface StorySkeletonProps {
   itemCount?: number;
 }
 
+const skeletonTheme = { bgcolor: 'grey.500' };
+
 const StorySkeleton: React.FC<StorySkeletonProps> = ({ itemCount = 1 }) => (
   <Stack
     direction="row"
@@ -14,16 +16,23 @@ const StorySkeleton: React.FC<StorySkeletonProps> = ({ itemCount = 1 }) => (
     sx={{ overflowX: 'auto', paddingX: 2 }}
   >
     {Array.from({ length: itemCount }).map((_, index) => (
-      <Box key={`skeleton-${index}`} minWidth={300} width="100%" py={'.5rem'} px={2}>
-        <Skeleton variant="text" width="100%" height={'2rem'} />
-        <Skeleton variant="text" width="80%" height={'2rem'} />
+      <Box
+        key={`skeleton-${index}`}
+        minWidth={300}
+        width="100%"
+        py={'.5rem'}
+        px={2}
+        sx={{ maxWidth: '100%', paddingY: 20, bgcolor: '#000' }}
+      >
+        <Skeleton sx={{ ...skeletonTheme }} variant="text" width="100%" height={'2rem'} />
+        <Skeleton sx={{ ...skeletonTheme }} variant="text" width="80%" height={'2rem'} />
         <Box display="flex" justifyContent={'space-between'} my={1}>
-          <Skeleton variant="text" width="50%" />
-          <Skeleton variant="text" width="30%" />
+          <Skeleton sx={{ ...skeletonTheme }} variant="text" width="50%" />
+          <Skeleton sx={{ ...skeletonTheme }} variant="text" width="30%" />
         </Box>
         <Box display="flex" justifyContent={'space-between'}>
-          <Skeleton variant="text" width="40%" />
-          <Skeleton variant="text" width="30%" />
+          <Skeleton sx={{ ...skeletonTheme }} variant="text" width="40%" />
+          <Skeleton sx={{ ...skeletonTheme }} variant="text" width="30%" />
         </Box>
       </Box>
     ))}
